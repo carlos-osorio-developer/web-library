21465f5f84f4e7f2774e44976d068040f59d9036
pountzas <nikos@pountzas.gr>
2021-07-06 01:37:45 +0300

    add remove buttons property

--- js/books.js
+++ js/books.js
@@ -1,29 +1,45 @@
-const propBook = { submitButton: document.getElementById('button'), 
-                   contentDiv: document.getElementById('content') };
+const propBook = {
+  submitButton: document.getElementById('button'),
+  contentDiv: document.getElementById('content'),
+  removeButtons: document.getElementsByClassName('remove'),
+};
 
-const metBook = {
+const createBook = {
   init() {
-    propBook.submitButton.addEventListener('click', metBook.updateStorage);
+    propBook.submitButton.addEventListener('click', createBook.updateStorage);
   },
 
   updateStorage() {
     localStorage.setItem('title', document.getElementById('title').value);
     localStorage.setItem('author', document.getElementById('author').value);
-    metBook.addBook();
+    createBook.addBook();
   },
 
   addBook() {
     const div = document.createElement('div');
+    div.className = 'books';
     const title = document.createElement('p');
+    const remove = document.createElement('button');
+    remove.textContent = 'remove';
+    remove.className = 'remove';
     title.textContent = localStorage.getItem('title');
     const author = document.createElement('p');
     author.textContent = localStorage.getItem('author');
     const separator = document.createElement('hr');
     div.appendChild(title);
     div.appendChild(author);
+    div.appendChild(remove);
     div.appendChild(separator);
+
     propBook.contentDiv.prepend(div);
   },
 };
 
-metBook.init();
+const deleteBook = {
+  init() {
+    // propBook.submitButton.addEventListener('click', createBook.updateStorage);
+  },
+};
+
+createBook.init();
+deleteBook.init();
