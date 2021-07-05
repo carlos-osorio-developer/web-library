3b509e74197edd4158493af4d51cd5fb5f4bee86
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-05 16:48:46 -0500

    Add title and author variables into localStorage
    Signed-off-by: carlos-osorio-developer <osorio.uis@gmail.com>

--- js/books.js
+++ js/books.js
@@ -1,3 +1,15 @@
 const propBook = { title: null, author: null, submitButton: document.getElementById('button')
-};
+}
 
+var metBook = {
+  init : function(){
+    propBook.submitButton.addEventListener('click', metBook.updateStorage);
+  },
+
+  updateStorage : function(){
+    localStorage.setItem('title', document.getElementById('title').value);
+    localStorage.setItem('author', document.getElementById('author').value);
+  }
+}
+
+metBook.init();
