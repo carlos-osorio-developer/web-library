d085af5c899b5816ba5a129bcdd29135d035b4fa
pountzas <nikos@pountzas.gr>
2021-07-07 19:08:21 +0300

    add style for title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-   purge : [
-        './*.html',
-    ],
-    darkMode: false, //or 'media' or 'class
-    theme: {
-        extend: {}
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+  purge: [
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
